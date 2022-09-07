7b5aadfb7b1c96e01614f6829c2c6143600cdba2
emmacintosh <emmammrussell@gmail.com>
2022-09-07 12:07:09 -0600

    Add first test to cypress file and two additional test skeletons

--- cypress/e2e/app_spec.cy.js
+++ cypress/e2e/app_spec.cy.js
@@ -1,5 +1,24 @@
+beforeEach(() => {
+  cy.visit('http://localhost:3000');
+});
+
 describe('Feedback Loop login flows', () => {
   it('Should confirm that true is equal to true', () => {
     expect(true).to.equal(true)
   });
-});
+
+  it('should see the controlled form when visiting the page', () => {
+    cy.get('h1').should('include.text', 'Sick Trick Wish List')
+    cy.get('select').should('exist')
+    cy.get('input').should('exist')
+    cy.get('button').should('exist')
+  })
+
+  it('should be able to type data into the form inputs', () => {
+
+  })
+
+  it('should be able to add a new trick to the DOM', () => {
+    
+  })
+})
