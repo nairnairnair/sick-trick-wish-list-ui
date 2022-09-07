2d5d791f9c9fdbe8c640b6a99d164219a95f7c82
emmacintosh <emmammrussell@gmail.com>
2022-09-07 11:45:35 -0600

    Add POSt request to send user inputted data to API, add handler functions to get info there

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,24 +6,22 @@ class App extends Component {
     super()
     this.state = {
       tricks: [],
-      // stance: [],
-      // name: [],
-      // obstacle: [],
-      // tutorial: [],
-      // id: []
+      stance: [],
+      name: [],
+      obstacle: [],
+      tutorial: [],
+      id: []
     }
   }
 
   mapTricks = () => {
     const mappedTricks = this.state.tricks.map((trick) => {
       return (
-      <div className="trickCard">
-      stance={trick.stance},
-      name={trick.name},
-      obstacle={trick.obstacle},
-      tutorial={trick.tutorial},
-      id={trick.id},
-      key={trick.id}
+      <div className="trickCard" id={trick.id} key={trick.id}>
+      Stance: {trick.stance},
+      Name: {trick.name},
+      Obstacle: {trick.obstacle},
+      Tutorial: {trick.tutorial},
       </div>)
     })
     console.log('mt', mappedTricks)
@@ -40,31 +38,53 @@ class App extends Component {
       this.mapTricks()
   }
 
+  handleChangeStance = (event) => {
+    this.setState({stance: event.target.value})
+  }
 
+  handleChangeName = (event) => {
+    this.setState({name: event.target.value})
+  }
+
+  handleChangeObstacle = (event) => {
+    this.setState({obstacle: event.target.value})
+  }
+
+  handleChangeTutorial = (event) => {
+    this.setState({tutorial: event.target.value})
+  }
+
+  handleClick = () => {
+    fetch('http://localhost:3001/api/v1/tricks', {
+    method: 'POST',
+    headers: {'Content-type': 'application/json'},
+    body: JSON.stringify( {stance: this.state.stance, name: this.state.name, obstacle: this.state.obstacle, tutorial: this.state.tutorial})
+    })
+  }
 
   render() {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
         <form>
-          <select className="stanceInput">
-            <option value="" disabled selected hidden>Choose your Stance</option>
+          <select className="stanceInput" onChange={this.handleChangeStance}>
+            <option value="" disabled hidden>Choose your Stance</option>
             <option value="Regular">Regular</option>
             <option value="Switch">Switch</option>
           </select>
-          <input className="nameInput" placeholder="Name of Trick"></input>
-          <select className="obstacleInput">
-            <option value="" disabled selected hidden>Choose your Obstacle</option>
+          <input className="nameInput" onChange={this.handleChangeName} placeholder="Name of Trick"></input>
+          <select className="obstacleInput" onChange={this.handleChangeObstacle}>
+            <option value="" disabled hidden>Choose your Obstacle</option>
             <option value="Flatground">Flatground</option>
             <option value="Ledge">Ledge</option>
             <option value="Rail">Rail</option>
             <option value="Stands">Stands</option>
             <option value="Pool">Pool</option>
           </select>
-          <input className="tutorialInput" placeholder="Link to Tutorial"></input>
-          <button className="sendIt">Send it!</button>
+          <input className="tutorialInput" onChange={this.handleChangeTutorial} placeholder="Link to Tutorial"></input>
+          <button className="sendIt" onClick={this.handleClick}>Send it!</button>
         </form>
-        <p>{this.mapTricks()}</p>
+        <div>{this.mapTricks()}</div>
       </div>
     );
   }
