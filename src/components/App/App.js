28a790e0d236b173fc881bd1c1497d19636683d5
emmacintosh <emmammrussell@gmail.com>
2022-09-07 10:39:14 -0600

    Add mapTricks function to render trick information to the DOM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,20 +5,48 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-
+      tricks: [],
+      // stance: [],
+      // name: [],
+      // obstacle: [],
+      // tutorial: [],
+      // id: []
     }
   }
 
-  componentDidMount(){
-    return fetch('http://localhost:3001/api/v1/tricks')
-      .then(response => response.json())
-      .catch(error => console.log(error))
+  mapTricks = () => {
+    const mappedTricks = this.state.tricks.map((trick) => {
+      return (
+      <div className="trickCard">
+      stance={trick.stance},
+      name={trick.name},
+      obstacle={trick.obstacle},
+      tutorial={trick.tutorial},
+      id={trick.id},
+      key={trick.id}
+      </div>)
+    })
+    console.log('mt', mappedTricks)
+    return mappedTricks
   }
+
+  componentDidMount(){
+    fetch('http://localhost:3001/api/v1/tricks')
+      .then(response => response.json())
+      .then(response => this.setState({
+        tricks: response,
+      }))
+      .catch(error => console.log(error))
+      this.mapTricks()
+  }
+
+
 
   render() {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
+        <p>{this.mapTricks()}</p>
       </div>
     );
   }
@@ -26,8 +54,3 @@ class App extends Component {
 
 export default App;
 
-//stance
-//name
-//obstacle
-//tutorial
-//id
