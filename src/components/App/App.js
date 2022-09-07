36b0c7fdad94439b725521ed27cf0e130986cf8a
emmacintosh <emmammrussell@gmail.com>
2022-09-07 09:31:19 -0600

    Initial commit, add constructor

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,12 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      
+    }
+  }
   render() {
     return (
       <div className="App">
